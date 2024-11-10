0b8856a15cc4a79cb6f309ad25c75ccacee5745a
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2024-11-10 18:45:03 +0200

    Repository added

---
+++ lib/repository/repository.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_home_work9/models/recipe.dart';
+
+class Repository {
+  static final List<Recipe> recipes = [
+    Recipe(
+      title: 'Борщ',
+      description: 'Традиційний український суп',
+      ingredients: ['Буряк', 'Картопля', 'Морква', 'Цибуля'],
+      instructions: [
+        'Підготувати овочі',
+        'Варити бульйон',
+        'Додати овочі',
+        'Варити до готовності'
+      ],
+      category: 'Обід',
+    ),
+    Recipe(
+      title: 'Салат Цезар',
+      description: 'Смачний салат з куркою',
+      ingredients: ['Курка', 'Салат', 'Сир пармезан', 'Соус'],
+      instructions: [
+        'Підготувати інгредієнти',
+        'Змішати все в мисці',
+        'Заправити соусом'
+      ],
+      category: 'Салати',
+    ),
+    Recipe(
+      title: 'Омлет',
+      description: 'Легкий та смачний сніданок',
+      ingredients: ['Яйця', 'Молоко', 'Сіль', 'Перець'],
+      instructions: ['Збити яйця', 'Додати молоко', 'Посолити', 'Посмажити'],
+      category: 'Сніданок',
+    ),
+  ];
+
+  static List<Recipe> getAllRecipes() {
+    return recipes;
+  }
+
+  static List<Recipe> getRecipesByCategory(String category) {
+    return recipes.where((recipe) => recipe.category == category).toList();
+  }
+}
